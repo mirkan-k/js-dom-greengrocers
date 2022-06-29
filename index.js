4e155209a39558476ad57c38d11f4eaa05479fc5
mirkan-k <mirkilinc@gmail.com>
2022-06-29 18:00:55 +0100

    added store shelf WIP

--- index.js
+++ index.js
@@ -53,3 +53,78 @@ const state = {
   ],
   cart: []
 };
+
+
+function setState() {
+  renderStore()
+}
+
+function renderStore() {
+  renderStoreItems()
+}
+
+function createItemIcon(item) {
+  const div = document.createElement('div')
+  div.classList.add('store--item-icon')
+  
+  const img = document.createElement('img')
+  img.setAttribute('src', `./assets/icons/${item.id}.svg`)
+  img.setAttribute('alt', item.name)
+  div.appendChild(img)
+
+  return div
+}
+
+function createItemButton() {
+  const button = document.createElement('button')
+  button.textContent = 'Add to cart'
+
+  return button
+}
+
+function createStoreItem(item) {
+  const li = document.createElement('li')
+
+  const icon = createItemIcon(item)
+  li.appendChild(icon)
+
+  const button = createItemButton()
+  li.appendChild(button)
+  
+  return li
+}
+
+function renderStoreItems() {
+  state.items.forEach(item => {
+    const storeItemList = document.querySelector('.store--item-list')
+
+    const storeItemLI = createStoreItem(item)
+    console.log(storeItemLI)
+
+    storeItemList.append(storeItemLI)
+  })
+}
+
+function createCartItem(item) {
+  const li = document.createElement('li')
+  const img = document.createElement('img')
+  const p = document.createElement('p')
+  const buttonRemove = document.createElement('button')
+  const quantity = document.createElement('span')
+  const buttonAdd = document.createElement('button')
+}
+
+function init() {
+  setState()
+}
+
+function addListeners() {
+
+}
+
+function run() {
+  init()
+  addListeners()
+}
+
+run()
