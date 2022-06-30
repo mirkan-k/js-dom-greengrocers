b3c58ef45c294549fd5e562cf1b34731260e7b24
mirkan-k <mirkilinc@gmail.com>
2022-06-30 16:40:01 +0100

    added full cart functionality

--- index.js
+++ index.js
@@ -55,14 +55,15 @@ const state = {
 };
 
 
-function setState() {
-  renderStore()
+function setState(cartItem) {
+  state.cart.push(cartItem)
 }
 
 function renderStore() {
   renderStoreItems()
 }
 
+// Creating & rendering store shelf items
 function createItemIcon(item) {
   const div = document.createElement('div')
   div.classList.add('store--item-icon')
@@ -75,9 +76,12 @@ function createItemIcon(item) {
   return div
 }
 
-function createItemButton() {
+function createItemButton(item) {
   const button = document.createElement('button')
   button.textContent = 'Add to cart'
+  button.addEventListener('click', () => {
+    addItemToCart(item)
+  })
 
   return button
 }
@@ -88,7 +92,7 @@ function createStoreItem(item) {
   const icon = createItemIcon(item)
   li.appendChild(icon)
 
-  const button = createItemButton()
+  const button = createItemButton(item)
   li.appendChild(button)
   
   return li
@@ -99,32 +103,161 @@ function renderStoreItems() {
     const storeItemList = document.querySelector('.store--item-list')
 
     const storeItemLI = createStoreItem(item)
-    console.log(storeItemLI)
+    // console.log(storeItemLI)
 
     storeItemList.append(storeItemLI)
   })
 }
 
+// Creating & rendering Cart items
+function createCartIcon(item) {
+  const img = document.createElement('img')
+  img.classList.add('cart--item-icon')
+  img.setAttribute('src', `./assets/icons/${item.id}.svg`)
+  img.setAttribute('alt', item.name)
+
+  return img
+}
+
+function createCartText(item) {
+  const p = document.createElement('p')
+  p.textContent = item.name
+  // console.log(item)
+
+  return p
+}
+
+function createCartDecrement(item) {
+  const decrement = document.createElement('button')
+  decrement.classList.add('quantity-btn', 'remove-btn', 'center')
+  decrement.textContent = '-'
+  decrement.addEventListener('click', () => {
+    removeItemFromCart(item)
+  })
+
+  return decrement
+}
+
+function removeItemFromCart(item) {
+  const itemIndex = getCartItemIndex(item)
+
+  if (item.quantity <= 1) {
+    state.cart.splice(itemIndex, 1)
+  } else {
+    item.quantity--
+  }
+
+  renderCartItem()
+  updateTotalPrice()
+}
+
+function createCartIncrement(item) {
+  const increment = document.createElement('button')
+  increment.classList.add('quantity-btn', 'add-btn', 'center')
+  increment.textContent = '+'
+  increment.addEventListener('click', () => {
+    addItemToCart(item)
+  })
+
+  return increment
+}
+
+function createCartQuantity(item) {
+  const quantity = document.createElement('span')
+  quantity.classList.add('quantity-text', 'center')
+  quantity.textContent = item.quantity
+
+  return quantity
+}
+
 function createCartItem(item) {
   const li = document.createElement('li')
-  const img = document.createElement('img')
-  const p = document.createElement('p')
-  const buttonRemove = document.createElement('button')
-  const quantity = document.createElement('span')
-  const buttonAdd = document.createElement('button')
+
+  const img = createCartIcon(item)
+  li.appendChild(img)
+
+  const p = createCartText(item)
+  li.appendChild(p)
+
+  const decrement = createCartDecrement(item)
+  li.appendChild(decrement)
+  
+  const quantity = createCartQuantity(item)
+  li.appendChild(quantity)
+  
+  const increment = createCartIncrement(item)
+  li.appendChild(increment)
+
+  return li
 }
 
+// Event Functions
+function clearCartList() {
+  const cart = document.querySelector('.cart--item-list')
+  cart.innerHTML = ''
+}
+
+function renderCartItem() {
+  clearCartList()
+  
+  state.cart.forEach(item => {
+    const cart = document.querySelector('.cart--item-list')
+    const cartItemLI = createCartItem(item)
+  
+    cart.appendChild(cartItemLI)
+  })
+}
+
+function getCartItemIndex(item) {
+  const itemIndex = state.cart.findIndex((cartItem => cartItem.id === item.id))
+
+  return itemIndex
+}
+
+function addItemToCart(item) {
+  const itemIndex = getCartItemIndex(item)
+  let quantity = 1
+
+  if (state.cart.some(e => e.id === item.id)) {
+    state.cart[itemIndex].quantity++
+  } else {
+    setState( 
+      {
+        ...item,
+        quantity: quantity
+      }
+    )
+  }
+  
+  console.log('cart', state.cart)
+  renderCartItem()
+  updateTotalPrice()
+}
+
+function updateTotalPrice() {
+  const totalNumber = document.querySelector('.total-number')
+
+  let totalPrice = 0
+
+  for (let i = 0; i < state.cart.length; i++) {
+    const item = state.cart[i];
+
+    totalPrice += (item.quantity * item.price)
+  }
+
+  totalNumber.textContent = `£${totalPrice.toFixed(2)}`
+  // console.log(totalPrice)
+}
+
+// INITIALIZE & RUN
 function init() {
-  setState()
-}
-
-function addListeners() {
-
+  renderStore()
+  updateTotalPrice()
 }
 
 function run() {
   init()
-  addListeners()
 }
 
 run()
+// console.log(state.cart)
